bf4b82d48793cf7c5815bbb52c5204e9003b8a02
MAB <20094576@mail.wit.ie>
2023-11-06 09:28:08 +0100

    base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: ' fleetwoodmab.github.io/app2/',
   plugins: [svelte()],
 })
